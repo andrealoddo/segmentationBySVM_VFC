ebe2471f075347db9aacfece13cb77e92d72e2f5
saimon <si.porcu3@studenti.unica.it>
2017-12-11 11:01:10 +0100

    modifica risultato finale per separare le cellule

--- getVEF.m
+++ getVEF.m
@@ -14,47 +14,6 @@ imageGrayDouble=double(imageGray);
 maxvalue=max(max(imageGrayDouble,[],2));
 imageGrayDouble =imageGrayDouble/maxvalue;
 
-%% Porzione di codice che esegue la watershed con marker. Usata solo per fare un paragone con il nostro progetto 
-% % % hy = fspecial('sobel');
-% % % hx = hy';
-% % % Iy = imfilter(double(imageGrayDouble), hy, 'replicate');
-% % % Ix = imfilter(double(imageGrayDouble), hx, 'replicate');
-% % % gradmag = sqrt(Ix.^2 + Iy.^2);
-% % % se = strel('disk', 5);
-% % % Io = imopen(imageGrayDouble, se);
-% % % Ie = imerode(imageGrayDouble, se);
-% % % Iobr = imreconstruct(Ie, imageGrayDouble);
-% % % Ioc = imclose(Io, se);
-% % % Iobrd = imdilate(Iobr, se);
-% % % Iobrcbr = imreconstruct(imcomplement(Iobrd), imcomplement(Iobr));
-% % % Iobrcbr = imcomplement(Iobrcbr);
-% % % fgm = imregionalmax(Iobrcbr);
-% % % I2 = imageGrayDouble;
-% % % I2(fgm) = 255;
-% % % se2 = strel(ones(5,5));
-% % % fgm2 = imclose(fgm, se2);
-% % % fgm3 = imerode(fgm2, se2);
-% % % fgm4 = bwareaopen(fgm3, 20);
-% % % I3 = imageGrayDouble;
-% % % I3(fgm4) = 255;
-% % % bw = imbinarize(Iobrcbr);
-% % % D = bwdist(bw);
-% % % DL = watershed(D);
-% % % bgm = DL == 0;
-% % % gradmag2 = imimposemin(gradmag, bgm | fgm4);
-% % % L = watershed(gradmag2);
-% % % I4 = imageGrayDouble;
-% % % I4(imdilate(L == 0, ones(3, 3)) | bgm | fgm4) = 255;
-% % % Lrgb = label2rgb(L, 'jet', 'w', 'shuffle');
-% % % figure
-% % % imshow(Lrgb)
-% % % title('Colored watershed label matrix (Lrgb)')
-
-% 
-% coloredLabels = label2rgb (L, 'hsv', 'k', 'shuffle');
-% 
-% figure; imshow(coloredLabels)
-
 % edge dell'immagine da passare al campo vettoriale. Il parametro varia tra
 % lo 0.05 e 0.07 dipende dal grado di rumorosit� dell'immagine
 f = edge(imageGrayDouble,'Sobel',0.06);
--- main.m
+++ main.m
@@ -47,16 +47,46 @@ testSet = [reshape(I, [size(I,1)*size(I,2), 3]), u(:), v(:)];
 angle=atan2(-v,u)*180/pi;
 angle=angle+180;
 angle1=bwdist(angle);
+Options=struct;
+  Options.Wedge=8;
+  Options.Wline=-8;
+  Options.Wterm=0;
+  Options.Sigma1=4;
+% estraggo l'energia e la trasmormo in scala di grigi
+Eext = ExternalForceImage2D(angle1,Options.Wline, Options.Wedge, Options.Wterm,Options.Sigma1);
+
+% prendo i nuclei
+Eextclear=imclearborder(Eext);
+risaltodeinuclei=imcomplement(Eext)+Eextclear;
+
+%  risaltodeinuclei=imfill(risaltodeinuclei,'holes');
+% binarizzo per separare il foreground dal background
+Eextbin = imbinarize(risaltodeinuclei,1.02);
+Eextbin=imopen(Eextbin,strel('disk',30));
+onlyLeuco=Eextbin.*angle;
+
 Pmed = ordfilt2(angle,18,true(5));
+Pmed1 = ordfilt2(onlyLeuco,18,true(5));
 
 diff = Pmed - angle;
+diff1 = Pmed1 - onlyLeuco;
 
 diff3 = diff;
+diff2 = diff1;
 
-diff3(diff<50)=0;
+diff2(diff1<30)=0;
+diff3(diff<30)=0;
+
+% %% Segmentazione con skel
+over=imoverlay(diff3,diff2,'red');
+diff3=uint8(diff3)-uint8(over(:,:,2));
+figure(2), imshow(diff3)
+
+diff3=imbinarize(diff3,'adaptive');
 
 %% Show visual result
 p = reshape(predicted, size(I,1), size(I,2));
+p(diff3==1)=0;
 figure(1), imshow(I);
-figure(2), imshow(imoverlay(p,diff3));
+figure(3), imshow(p);
 
